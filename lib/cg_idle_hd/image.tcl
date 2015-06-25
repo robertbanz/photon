f3f52dbee9f80b4194f0f56bc6af51071f51f8a4
game <game@np.h.nofocus.org>
2015-06-25 12:03:59 -0700

    video clip support in idle screen

---
+++ lib/cg_idle_hd/image.tcl
@@ -0,0 +1,27 @@
+
+proc run { p } {
+    variable advertimage
+    variable nextevent
+
+    canvas $p.advert -width [ winfo reqwidth $p ] -height [ winfo reqheight $p ]
+
+    set advertimage [ image create photo -file ${Conf::gamepath}/images/cg_idle_hd/image.tif ]
+    
+    $p.advert create image 0 0 -image $advertimage -anchor nw
+    
+    place $p.advert -relx 0.0 -rely 0.0
+    
+#    bind $p.advert <<Destroy>> "::Idlescreen::end"
+
+    # wait 1 minute
+    set nextevent [ after 2000 "::dm_next_IDLE $p" ]
+}
+
+proc end { p } {
+    variable nextevent
+    variable advertimage
+    catch [ list after cancel $nextevent ]
+    catch [ list image delete $advertimage ]
+    destroy $p
+}
+
---
+++ lib/cg_idle_hd/video.tcl
@@ -0,0 +1,73 @@
+
+set mplayer_pid 0
+
+set clipdir "/usr/photon/lib/clips/"
+
+proc pick_clip { dir } {
+  set clips [ glob ${dir}/*.mp4 ]
+  set which [ expr { int(rand() * [llength $clips]) }]
+  return [ lindex $clips $which ]
+}
+
+proc run { p } {
+  variable mplayer_pid
+  variable check_done_event
+  variable clipdir
+  
+  canvas $p.video -width [ winfo reqwidth $p ] -height [ winfo reqheight $p ]
+
+  image create photo video_background_image -file \
+    ${::Conf::gamepath}/images/cg_idle_hd/video_background.tif
+  
+  $p.video create image 0 0 -image video_background_image -anchor nw
+
+  # create a widget to place our video in
+  label $p.video.clip -height 540 -width 720
+  $p.video create window 381 90 -height 540 -width 720 -anchor nw -window $p.video.clip
+  
+  place $p.video -relx 0.0 -rely 0.0
+
+  set mplayer_pid [fork]
+
+  set window_id [ winfo id $p.video.clip ]
+
+  set clip [ pick_clip $clipdir ]
+
+  if { $mplayer_pid == 0 } {
+    execl "mplayer" [ list "-nosound" "-wid" $window_id ${clip} "-frames" 100 ]
+    #execl "mplayer" [ list "-nosound" ${clip} ]
+    kill [id process]
+  }
+  
+#  bind $p.video <<Destroy>> "::Idlescreen::end"
+  
+  set check_done_event [ after 1000 [ list ::Idlescreen::check_done $p ] ]
+}
+
+proc check_done { root } {
+  variable mplayer_pid
+  if { $mplayer_pid != 0 } {
+    set result [ wait -nohang $mplayer_pid ]
+    if { [ llength $result ] != 0 } {
+      set mplayer_pid 0
+      ::dm_next_IDLE $root
+    } else {
+      set check_done_event [ after 1000 [ list ::Idlescreen::check_done $root ] ]
+    }
+  }
+}
+
+proc end { p } {
+  variable check_done_event
+  variable mplayer_pid
+  catch [ list after cancel $check_done_event ]
+  catch [ image delete video_background_image ]
+
+  if { $mplayer_pid != 0 } {
+    kill $mplayer_pid
+    wait $mplayer_pid
+  }
+  
+  destroy $p
+}
+
