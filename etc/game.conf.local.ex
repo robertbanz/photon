3a9010a0caf8b15ae477a1a0f046c835312c53f8
Robert Banz <rob@nofocus.org>
2016-04-11 23:36:52 -0400

    Back in business.

---
+++ etc/game.conf.local.ex
@@ -0,0 +1,8 @@
+# local machine game configuration file
+set server_name game
+set dbhost game
+set ettty /dev/cuaU0
+set mixer_device /dev/mixer0
+#set cr_server_name 192.168.3.78
+#set cr_listenport 23
+set cr_tty /dev/cuaU0
