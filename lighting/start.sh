fa3f00fcea8514876c8c1b2e144e836880823b18
robert banz <rob@nofocus.org>
2018-08-03 16:26:19 -0700

    Updates to lighting startup script

--- lighting/start.sh
+++ lighting/start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/Applications/QLC+.app/Contents/MacOS/qlcplus -o /Users/game/photon/lighting/laurelv1.qxw -p
+ln -s $HOME/photon/lighting/fixtures/Stage-Right-Stage-Beam.qxf "$HOME/Library/Application Support/QLC+/Fixtures/Stage-Right-Stage-Beam.qxf"
+/Applications/QLC+.app/Contents/MacOS/qlcplus -o $HOME/photon/lighting/laurelv1.qxw -p
